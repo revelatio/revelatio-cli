a83306adfdc9d1da1ee86fac5aa153bd9629b5cd
gorums <acksecurity@hotmail.com>
2017-01-04 17:30:27 -0500

    add the create option in the cli

--- index.js
+++ index.js
@@ -3,4 +3,17 @@
 
 const program = require('commander');
 
-console.log('Hello, world!');
+let createRevelatioJson = () => {
+  console.log("Create Revelario Json")
+}
+
+program
+  .version('0.1.0')
+  .command('create')
+  .description('Create a revelatio parent project')
+  .action(createRevelatioJson);
+
+program.parse(process.argv);
+if (program.args.length === 0 || !(program.args[0] instanceof Object)) {
+  program.help();
+}
