e7130b774460ca17a9ce9fc42a59fd1426590817
gorums <acksecurity@hotmail.com>
2017-01-05 00:34:06 -0500

    add pretty prompt

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 #!/usr/bin/env node
 'use strict';
 
-const program = require('commander');
+const fs = require('fs');
 
-let createRevelatioJson = () => {
-  console.log("Create Revelario Json")
+const program = require('commander');
+const co = require('co');
+const prompt = require('co-prompt');
+const chalk = require('chalk');
+
+const pkg = require('./package');
+
+var revelation = require('./template/revelatio');
+
+let createRevelatioJson = (domain) => {
+  revelation.domain = domain;
+  fs.writeFile("./revelatio.json", JSON.stringify(revelation), function(err) {
+
+    if(err) {
+        return console.log(chalk.bold.red("[-] " + err));
+    }
+
+    console.log(chalk.bold.green("[+] The revelatio.json was saved!"));
+    process.exit(0);
+  });
+}
+
+let prettyPrompt = () => {
+  co(function *() {
+    console.log(chalk.bold.blue("[ ] Creating revelatio base project"));
+    console.log(chalk.bold.blue(`[ ] v${pkg.version}`));
+
+    const domain = yield prompt(chalk.bold.cyan('[*] domain: '));
+    createRevelatioJson(domain);
+  });
 }
 
 program
   .version('0.1.0')
   .command('create')
   .description('Create a revelatio parent project')
-  .action(createRevelatioJson);
+  .action(prettyPrompt);
 
 program.parse(process.argv);
-if (program.args.length === 0 || !(program.args[0] instanceof Object)) {
+if (program.args.length === 0) {
   program.help();
 }
