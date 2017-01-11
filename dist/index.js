fc6b676dde074f27b0e2dc15a2f0f25c02f00d2d
Ernesto Freyre <ernesto@bettercompany.co>
2017-01-11 01:30:56 -0800

    updated dist

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,23 @@
 #!/usr/bin/env node
-'use strict'
+'use strict';
 
-var _commander = require('commander')
+var _commander = require('commander');
 
-var _commander2 = _interopRequireDefault(_commander)
+var _commander2 = _interopRequireDefault(_commander);
 
-var _package = require('../package.json')
+var _package = require('../package.json');
 
-var _index = require('./commands/index')
+var _index = require('./commands/index');
 
-var _index2 = _interopRequireDefault(_index)
+var _index2 = _interopRequireDefault(_index);
 
-function _interopRequireDefault (obj) { return obj && obj.__esModule ? obj : { default: obj } }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _commander2.default.version(_package.version);
 
-(0, _index2.default)(_commander2.default)
+(0, _index2.default)(_commander2.default);
 
-_commander2.default.parse(process.argv)
+_commander2.default.parse(process.argv);
 if (_commander2.default.args.length === 0) {
-  _commander2.default.help()
+  _commander2.default.help();
 }
